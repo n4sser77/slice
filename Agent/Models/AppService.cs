f763526fe8b80139a785b75748add4430617a6b0
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2026-03-14 19:47:06 +0100

    Portmanager integration with ProcessManager, control of what ports are available in the range 5001-5050

--- Agent/Models/AppService.cs
+++ Agent/Models/AppService.cs
@@ -1,4 +1,17 @@
 namespace Agent.Models;
 
-public record AppService(int Id, string Title, DateOnly? DateCreated = null, bool IsRunning = false);
+public class AppService
+{
+    public AppService(int Id, string Title, DateOnly? DateCreated = null, bool IsRunning = false)
+    {
+        id = Id;
+        title = Title;
+        dateCreated = DateCreated;
+        isRunning = IsRunning;
+    }
+    private readonly int id;
+    private readonly string title;
+    private readonly DateOnly? dateCreated;
+    private readonly bool isRunning;
 
+}
--- Agent/Models/SystemdService.cs
+++ Agent/Models/SystemdService.cs
@@ -3,6 +3,7 @@ namespace Agent.Models;
 public class SystemdService
 {
     public string Unit { get; set; } = "";
+    public string Loaded { get; set; } = "";
     public string Active { get; set; } = "";
     public string Sub { get; set; } = "";
     public string Description { get; set; } = "";
